8a52e0a2975eb52137283a4e088a5ac4bd77ee0a
jvanpiccs <9990398+jvanpiccs@users.noreply.github.com>
2022-01-02 17:53:24 -0300

    cambio en el panel

--- src/webparts/webcomIncidentes/components/WebcomIncidentes.tsx
+++ src/webparts/webcomIncidentes/components/WebcomIncidentes.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import styles from './WebcomIncidentes.module.scss';
-import { Dropdown, IconButton, Separator, Stack, Text } from '@fluentui/react';
+import {
+  ActionButton,
+  Dropdown,
+  IconButton,
+  Panel,
+  Separator,
+  Stack,
+  Text,
+} from '@fluentui/react';
 import useGetItems from './getItems';
 import { Incidente } from './Incidente';
 import { useBoolean } from '@fluentui/react-hooks';
 import { opcionesEstado } from './opcionesEstado';
+import { PrimaryButton } from '@microsoft/office-ui-fabric-react-bundle';
 
 export interface IWebcomIncidentesProps {
   description: string;
@@ -12,7 +20,8 @@ export interface IWebcomIncidentesProps {
 
 export const WebcomIncidentes: React.FunctionComponent<IWebcomIncidentesProps> =
   (props: React.PropsWithChildren<IWebcomIncidentesProps>) => {
-    const [filter, { toggle: toggleFilter }] = useBoolean(true);
+    const [isOpenFilter, {setTrue:openFilter, setFalse: dissmissFilter, toggle: toggleFilter }] =
+      useBoolean(true);
 
     const [estado, setEstado] = React.useState(opcionesEstado[0]);
 
@@ -20,31 +29,35 @@ export const WebcomIncidentes: React.FunctionComponent<IWebcomIncidentesProps> =
 
     return (
       <Stack>
-        <Stack horizontal>
+        <Stack horizontal horizontalAlign='space-between' verticalAlign='center'>
           <Text variant='large'>Incidentes</Text>
-          <IconButton
+          <ActionButton
+            text='Filtros'
             iconProps={{ iconName: 'Filter' }}
             onClick={() => toggleFilter()}
           />
         </Stack>
-        <div style={{ display: !filter && 'none' }}>
-          <Text>Filtros</Text>
-          <Stack horizontal>
-            <Dropdown
-              placeholder='Estado'
-              label='Estado'
-              options={opcionesEstado}
-              style={{minWidth:'150px'}}
-              defaultSelectedKey={estado.key}
-              multiSelect={false}
-              onChange={(ev,item)=>setEstado(item)}
-            />
-          </Stack>
-        </div>
-        <Separator />
-        {incidentes.length > 0
-          ? incidentes.map((i) => <Incidente item={i} />)
-          : 'No hay incidentes'}
+        <Panel
+          isLightDismiss
+          isBlocking={false}
+          isOpen={isOpenFilter}
+          onDismiss={dissmissFilter}
+          closeButtonAriaLabel='Cerrar'
+          headerText='Filtros de Incidentes'
+        >
+          <Dropdown
+            placeholder='Estado'
+            label='Estado'
+            options={opcionesEstado}
+            style={{ minWidth: '150px' }}
+            defaultSelectedKey={estado.key}
+            multiSelect={false}
+            onChange={(ev, item) => setEstado(item)}
+          />
+          <PrimaryButton text='Cerrar' onClick={() => dissmissFilter()} />
+        </Panel>
+        {incidentes.length > 0 && incidentes.map((i) => <Incidente item={i} />)}
+        {incidentes.length = 0 && `No hay incidentes en estado ${estado.text}`}
       </Stack>
     );
   };
